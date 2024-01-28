c1b438a188eafeac149016520b83dde223d7cf77
Henrique Fernandes <rickfernandes17@gmail.com>
2024-01-27 22:51:23 -0300

    Controler Unidade de Medida

--- Controllers/UnidadesMedidasController.cs
+++ Controllers/UnidadesMedidasController.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BackEndCurso.Models;
 
 namespace BackEndCurso.Controllers
 {
-    public class UnidadesMedidasController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnidadesMedidasController : ControllerBase
     {
         private readonly EstoqueTreinamentoContext _context;
 
@@ -18,145 +20,118 @@ namespace BackEndCurso.Controllers
             _context = context;
         }
 
-        // GET: UnidadesMedidas
-        public async Task<IActionResult> Index()
+        // GET: api/UnidadesMedidas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UnidadesMedida>>> GetUnidadesMedidas()
         {
-              return _context.UnidadesMedidas != null ? 
-                          View(await _context.UnidadesMedidas.ToListAsync()) :
-                          Problem("Entity set 'EstoqueTreinamentoContext.UnidadesMedidas'  is null.");
+          if (_context.UnidadesMedidas == null)
+          {
+              return NotFound();
+          }
+            return await _context.UnidadesMedidas.ToListAsync();
         }
 
-        // GET: UnidadesMedidas/Details/5
-        public async Task<IActionResult> Details(string id)
+        // GET: api/UnidadesMedidas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UnidadesMedida>> GetUnidadesMedida(string id)
         {
-            if (id == null || _context.UnidadesMedidas == null)
-            {
-                return NotFound();
-            }
-
-            var unidadesMedida = await _context.UnidadesMedidas
-                .FirstOrDefaultAsync(m => m.Sigla == id);
-            if (unidadesMedida == null)
-            {
-                return NotFound();
-            }
-
-            return View(unidadesMedida);
-        }
-
-        // GET: UnidadesMedidas/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: UnidadesMedidas/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Sigla,Descricao,CasasDecimais,Ativa,CadastradoUsuario,CadastradoData,AlteradoUsuario,AlteradoData")] UnidadesMedida unidadesMedida)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(unidadesMedida);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(unidadesMedida);
-        }
-
-        // GET: UnidadesMedidas/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null || _context.UnidadesMedidas == null)
-            {
-                return NotFound();
-            }
-
+          if (_context.UnidadesMedidas == null)
+          {
+              return NotFound();
+          }
             var unidadesMedida = await _context.UnidadesMedidas.FindAsync(id);
+
             if (unidadesMedida == null)
             {
                 return NotFound();
             }
-            return View(unidadesMedida);
+
+            return unidadesMedida;
         }
 
-        // POST: UnidadesMedidas/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Sigla,Descricao,CasasDecimais,Ativa,CadastradoUsuario,CadastradoData,AlteradoUsuario,AlteradoData")] UnidadesMedida unidadesMedida)
+        // PUT: api/UnidadesMedidas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUnidadesMedida(string id, UnidadesMedida unidadesMedida)
         {
             if (id != unidadesMedida.Sigla)
             {
-                return NotFound();
+                return BadRequest();
             }
 
-            if (ModelState.IsValid)
+            _context.Entry(unidadesMedida).State = EntityState.Modified;
+
+            try
             {
-                try
-                {
-                    _context.Update(unidadesMedida);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UnidadesMedidaExists(unidadesMedida.Sigla))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                await _context.SaveChangesAsync();
             }
-            return View(unidadesMedida);
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UnidadesMedidaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // GET: UnidadesMedidas/Delete/5
-        public async Task<IActionResult> Delete(string id)
+        // POST: api/UnidadesMedidas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UnidadesMedida>> PostUnidadesMedida(UnidadesMedida unidadesMedida)
         {
-            if (id == null || _context.UnidadesMedidas == null)
+          if (_context.UnidadesMedidas == null)
+          {
+              return Problem("Entity set 'EstoqueTreinamentoContext.UnidadesMedidas'  is null.");
+          }
+            _context.UnidadesMedidas.Add(unidadesMedida);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (UnidadesMedidaExists(unidadesMedida.Sigla))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetUnidadesMedida", new { id = unidadesMedida.Sigla }, unidadesMedida);
+        }
+
+        // DELETE: api/UnidadesMedidas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnidadesMedida(string id)
+        {
+            if (_context.UnidadesMedidas == null)
             {
                 return NotFound();
             }
-
-            var unidadesMedida = await _context.UnidadesMedidas
-                .FirstOrDefaultAsync(m => m.Sigla == id);
+            var unidadesMedida = await _context.UnidadesMedidas.FindAsync(id);
             if (unidadesMedida == null)
             {
                 return NotFound();
             }
 
-            return View(unidadesMedida);
-        }
-
-        // POST: UnidadesMedidas/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            if (_context.UnidadesMedidas == null)
-            {
-                return Problem("Entity set 'EstoqueTreinamentoContext.UnidadesMedidas'  is null.");
-            }
-            var unidadesMedida = await _context.UnidadesMedidas.FindAsync(id);
-            if (unidadesMedida != null)
-            {
-                _context.UnidadesMedidas.Remove(unidadesMedida);
-            }
-            
+            _context.UnidadesMedidas.Remove(unidadesMedida);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return NoContent();
         }
 
         private bool UnidadesMedidaExists(string id)
         {
-          return (_context.UnidadesMedidas?.Any(e => e.Sigla == id)).GetValueOrDefault();
+            return (_context.UnidadesMedidas?.Any(e => e.Sigla == id)).GetValueOrDefault();
         }
     }
 }
